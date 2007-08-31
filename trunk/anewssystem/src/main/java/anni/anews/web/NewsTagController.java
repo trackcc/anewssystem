c1173684775b94b01a6f9b47d8af216dd52e2af5
xyz20003 <xyz20003@3033d2cf-8137-0410-831d-77a957d129e5>
2007-08-31 19:16:39 +0000

    在newstag上演示两种grid,inlineEditor和dialog

--- trunk/anewssystem/src/main/java/anni/anews/web/NewsTagController.java
+++ trunk/anewssystem/src/main/java/anni/anews/web/NewsTagController.java
@@ -5,6 +5,7 @@ import anni.anews.domain.NewsTag;
 import anni.anews.manager.NewsTagManager;
 
 import anni.core.web.prototype.BaseLongController;
+import anni.core.web.prototype.StreamView;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -22,5 +23,63 @@ public class NewsTagController extends BaseLongController<NewsTag, NewsTagManage
     public NewsTagController() {
         setEditView("/anews/newstag/editNewsTag");
         setListView("/anews/newstag/listNewsTag");
+    }
+
+    /** * inlineManage. */
+    public void inlineManage() {
+        mv.setViewName("anews/newstag/inlineManage");
+    }
+
+    /** * dialogManage. */
+    public void dialogManage() {
+        mv.setViewName("anews/newstag/dialogManage");
+    }
+
+    /** * insertRow. */
+    public void insertRow() throws Exception {
+        String name = getStrParam("name", "");
+
+        if (!"".equals(name)) {
+            getEntityDao().createOrGet(name);
+        }
+
+        mv.setView(new StreamView("application/json"));
+        response.getWriter().print("{success:true,info:'success'}");
+    }
+
+    /** * updateRow. */
+    public void updateRow() throws Exception {
+        long id = getLongParam("id", -1L);
+        logger.info(id);
+
+        NewsTag newsTag = getEntityDao().get(id);
+
+        if (newsTag != null) {
+            String name = getStrParam("name", "");
+            logger.info(name);
+            newsTag.setName(name);
+            getEntityDao().save(newsTag);
+        }
+
+        logger.info(newsTag);
+
+        mv.setView(new StreamView("application/json"));
+        response.getWriter().print("{success:true,info:'success'}");
+    }
+
+    /** * loadData. */
+    public void loadData() throws Exception {
+        long id = getLongParam("id", -1L);
+
+        if (id != -1L) {
+            NewsTag newsTag = getEntityDao().get(id);
+            String json = "[{id:" + id + ",name:'" + newsTag.getName()
+                + "'}]";
+            logger.info(json);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().print(json);
+        }
+
+        mv.setView(new StreamView("application/json"));
     }
 }
